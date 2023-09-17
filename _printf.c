54e712562058b5e739c8576e82fa5e25ab5aa19a
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2023-09-17 13:00:25 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,43 +1,34 @@
 #include "main.h"
+
 /**
- * _printf - Print all this parameters
- * @format: input
- *
- * Description: function that prints output
- *
- * Return: The output character or num
+ * _printf - implementation of the inbuilt printf
+ * @format: the format specifier
+ * Return: the formated string
  */
+
 int _printf(const char *format, ...)
 {
-	int x = 0, o_p = 0;
-	char *ptr = (char *) format, *output_p;
-	int (*ptr_func)(va_list, char *, int);
-	va_list vlist;
+	int printed = 0;
 
-	if (!format)
-		return (-1);
-	va_start(vlist, format);
-	output_p = malloc(sizeof(char) * SIZE);
-	if (!output_p)
-		return (1);
-	while (format[x])
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format != '\0')
 	{
-		if (format[x] != '%')
-			output_p[o_p] = format[x], o_p++;
-		else if (s_trlen(ptr) != 1)
+		if (*format == '%')
 		{
-			ptr_func = format_type(++ptr);
-			if (!ptr_func)
-				output_p[o_p] = format[x], o_p++;
-			else
-				o_p = ptr_func(vlist, output_p, o_p), x++;
+			format++;
+			printed = selector(format, args, printed);
+			format++;
 		}
 		else
-			o_p = -1;
-		x++, ptr++;
+		{
+			_putchar(*format);
+			printed++;
+			format++;
+		}
 	}
-	va_end(vlist);
-	write(1, output_p, o_p);
-	free(output_p);
-	return (o_p);
+	va_end(args);
+	return (printed);
 }
