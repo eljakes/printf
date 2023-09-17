a753c48f8489d9cfce78d0c779faa309288e9578
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2023-09-17 12:57:49 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,45 +1,43 @@
 #include "main.h"
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * _printf - Print all this parameters
+ * @format: input
+ *
+ * Description: function that prints output
+ *
+ * Return: The output character or num
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
+	int x = 0, o_p = 0;
+	char *ptr = (char *) format, *output_p;
+	int (*ptr_func)(va_list, char *, int);
+	va_list vlist;
 
-	va_list args;
-	int i = 0, j, len = 0;
-
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (!format)
 		return (-1);
-
-Here:
-	while (format[i] != '\0')
+	va_start(vlist, format);
+	output_p = malloc(sizeof(char) * SIZE);
+	if (!output_p)
+		return (1);
+	while (format[x])
 	{
-		j = 13;
-		while (j >= 0)
+		if (format[x] != '%')
+			output_p[o_p] = format[x], o_p++;
+		else if (s_trlen(ptr) != 1)
 		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
+			ptr_func = format_type(++ptr);
+			if (!ptr_func)
+				output_p[o_p] = format[x], o_p++;
+			else
+				o_p = ptr_func(vlist, output_p, o_p), x++;
 		}
-		_putchar(format[i]);
-		len++;
-		i++;
+		else
+			o_p = -1;
+		x++, ptr++;
 	}
-	va_end(args);
-	return (len);
+	va_end(vlist);
+	write(1, output_p, o_p);
+	free(output_p);
+	return (o_p);
 }
